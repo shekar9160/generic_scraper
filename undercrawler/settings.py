2c5f0ef96cd2bbf922ecb2b95aa30ac600453840
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-03-09 13:48:57 +0300

    Fix reading gzipped splash response
    Change middleware order so that CompressionMiddleware is
    executed on the response before HHSplashMiddleware.

--- undercrawler/settings.py
+++ undercrawler/settings.py
@@ -9,7 +9,7 @@ DEPTH_LIMIT = 20
 SPLASH_URL = 'http://0.0.0.0:8050'
 
 DOWNLOADER_MIDDLEWARES = {
-    'undercrawler.middleware.HHSplashMiddleware': 725,
+    'undercrawler.middleware.HHSplashMiddleware': 585,
 }
 
 DUPEFILTER_CLASS = 'scrapyjs.SplashAwareDupeFilter'
