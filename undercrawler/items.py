aef9d663b126d0f7bd3ce11dce4f1d232e345848
Mikhail Korobov <kmike84@gmail.com>
2016-03-16 05:28:36 +0500

    prefer pagination links

--- undercrawler/items.py
+++ undercrawler/items.py
@@ -4,9 +4,15 @@ import scrapy
 class PageItem(scrapy.Item):
     url = scrapy.Field()
     text = scrapy.Field()
+    is_page = scrapy.Field()
+    depth = scrapy.Field()
 
     def __repr__(self):
-        return repr({'url': self['url']})
+        return repr({
+            'url': self['url'],
+            'is_page': self['is_page'],
+            'depth': self['depth'],
+        })
 
 
 class FormItem(scrapy.Item):
