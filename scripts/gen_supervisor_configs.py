29bcebe179b932a926ed84673f1422c347d5e9b4
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-03-17 19:33:51 +0300

    autostart = false to avoid auto-start of the crawl

--- scripts/gen_supervisor_configs.py
+++ scripts/gen_supervisor_configs.py
@@ -9,6 +9,7 @@ environment = LANG="en_US.UTF-8", USER="{user}"
 directory = {root}
 stopsignal = INT
 stopwaitsecs = 360
+autostart = false
 command = {scrapy} crawl base -a url={url} -s LOG_FILE={log} -o {out} -s JOBDIR={job} {extra}
 log_stderr = true
 '''
