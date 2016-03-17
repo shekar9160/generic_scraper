251fd736d02ceec90027209ff6a8089da1a1bd16
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-03-17 12:58:46 +0300

    Add an option to skip duplicates check
    This check is by far the slowest.

--- scripts/crawl_stats.py
+++ scripts/crawl_stats.py
@@ -10,25 +10,29 @@ from scripts.utils import item_reader, get_too_common_shingles, get_min_hash
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('cralwer_out')
-    parser.add_argument('--show')
+    parser.add_argument('--show', help='show urls for given key')
+    parser.add_argument('--skip-unique', action='store_true',
+                        help='skip unique check')
     args = parser.parse_args()
 
-    params = {'show': args.show}
-    if os.path.isdir(args.cralwer_out):
-        for filename in os.listdir(args.cralwer_out_dir):
-            with open(os.path.join(args.cralwer_out_dir, filename)) as f:
+    params = vars(args)
+    cralwer_out = params.pop('cralwer_out')
+    if os.path.isdir(cralwer_out):
+        for filename in os.listdir(cralwer_out):
+            with open(os.path.join(cralwer_out, filename)) as f:
                 print()
                 print(filename)
                 print_stats(f, **params)
     else:
-        with open(args.cralwer_out) as f:
+        with open(cralwer_out) as f:
             print_stats(f, **params)
 
 
-def print_stats(f, show=None):
+def print_stats(f, show=None, skip_unique=False):
     meta_counts = Counter()
-    lsh = LSH(threshold=0.9, num_perm=128)
-    too_common = get_too_common_shingles(f, limit=1000)
+    if not skip_unique:
+        lsh = LSH(threshold=0.9, num_perm=128)
+        too_common = get_too_common_shingles(f, limit=1000)
     for i, item in enumerate(item_reader(f)):
         meta_counts.update(['items'])
         for key, value in item['extracted_metadata'].items():
@@ -40,10 +44,11 @@ def print_stats(f, show=None):
                 meta_counts.update([key])
                 if key == show:
                     print(item['url'])
-        min_hash = get_min_hash(item, too_common)
-        if not lsh.query(min_hash):
-            meta_counts.update(['unique_items'])
-        lsh.insert('item_{}'.format(i), min_hash)
+        if not skip_unique:
+            min_hash = get_min_hash(item, too_common)
+            if not lsh.query(min_hash):
+                meta_counts.update(['unique_items'])
+            lsh.insert('item_{}'.format(i), min_hash)
     for k, v in sorted(meta_counts.items()):
         print(k.ljust(20), v)
 
