e05685fcdbd59385711cb45bd973d5c527d26798
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-03-22 18:31:07 +0300

    Keep a queue of initial requests for dupe detector
    They are important to know which content is common on all pages
    and which is unique. This is not a problem because we can only
    detect duplicates after seeing a fair number of example pages.

--- undercrawler/middleware/avoid_dup_content.py
+++ undercrawler/middleware/avoid_dup_content.py
@@ -20,7 +20,12 @@ class AvoidDupContentMiddleware:
     AVOID_DUP_CONTENT_ENABLED = True
     '''
     def __init__(self):
-        self.dupe_predictor = DupePredictor()
+        self.dupe_predictor = None
+        # We initialize dupe detector only after gathering enough pages,
+        # it needs them for better duplicate detection, to know which content
+        # is common to a lot of pages, and which is unique.
+        self.initial_queue = []  # (url, text)
+        self.initial_queue_limit = 300
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -29,14 +34,27 @@ class AvoidDupContentMiddleware:
         return cls()
 
     def process_request(self, request, spider):
-        dupe_prob = self.dupe_predictor.get_dupe_prob(request.url)
-        # TODO - lower priority for some requests
-        if dupe_prob > 0.98:
-            logger.debug('Ignoring a likely duplicate %s with prob %3.f',
-                         request.url, dupe_prob)
-            raise IgnoreRequest
+        if self.dupe_predictor:
+            dupe_prob = self.dupe_predictor.get_dupe_prob(request.url)
+            # TODO - lower priority for some requests
+            if dupe_prob > 0.98:
+                logger.debug('Ignoring a likely duplicate %s with prob %3.f',
+                            request.url, dupe_prob)
+                raise IgnoreRequest
 
     def process_response(self, request, response, spider):
-        self.dupe_predictor.update_model(
-            url=response.url, text=extract_text(response))
+        url, text = response.url, extract_text(response)
+        if self.dupe_predictor:
+            self.dupe_predictor.update_model(url, text)
+        else:
+            self.initial_queue.append((url, text))
+            if len(self.initial_queue) >= self.initial_queue_limit:
+                logger.debug(
+                    'Gathered enough intitial pages, building DupePredictor')
+                self.dupe_predictor = DupePredictor(
+                    texts_sample=[text for _, text in self.initial_queue])
+                # Update model with all the pages we have missed
+                for url, text in self.initial_queue:
+                    self.dupe_predictor.update_model(url, text)
+                self.initial_queue = None
         return response
