2869b6cfbf75eb3576478dcce32d5a3a4a7f7506
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-04-13 15:27:42 +0300

    Proper way to handle error with file download

--- undercrawler/directives/download.lua
+++ undercrawler/directives/download.lua
@@ -6,7 +6,14 @@ function main(splash)
   end
 
   response = splash:http_get{splash.args.url, headers=splash.args.headers}
-  assert(response.ok) -- TODO?
-
-  return response.body
+  if response.ok then
+    return response.body
+  else
+    return {
+      ok = response.ok,
+      body = response.body,
+      http_status = response.status,
+      headers = response.headers,
+    }
+  end
 end
